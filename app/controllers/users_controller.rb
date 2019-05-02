c7f36af8fb5ff9411720ce398ea9ce21c263bfd2
jnevra <49840764+jnevra@users.noreply.github.com>
2019-05-03 03:04:23 +0700

    login l3

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # Handle a successful save.
+      else
+      render 'new'
+    end
+  end
+  def edit
+    @user = User.find( params[:id] )
+  end
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
